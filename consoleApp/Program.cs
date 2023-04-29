19d7ac45cb5aea621e9afc0d94e8f7defa487e84
kitdim <kitaykin9808@gmail.com>
2023-04-29 11:12:25 +0300

    Добавить структуру проекта, основные методы и классы

---
+++ consoleApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using static System.Console;
+using TranslateLiblary;
+internal class Program
+{
+    private static void Main()
+    {
+        Translate tutor = new();
+        tutor.AddWords("dog", "собака");
+        tutor.AddWords("cat", "кошка");
+        tutor.AddWords("door", "дверь");
+
+        while (true)
+        {
+            var checkWord = tutor.GetRandomEngWord();
+            Write("Как переводится слово {0}?: ", checkWord);
+            var answer = ReadLine() ?? "пустая строка";
+
+            if (tutor.CheckWord(checkWord, answer))
+            {
+                WriteLine("Правильно");
+            }
+            else
+            {
+                var currectAnswer = tutor.Translating(checkWord);
+                WriteLine("Неверно, слово {0} переводится как {1}.", checkWord, currectAnswer);
+            }
+
+            Clear();
+        }
+    }
+}
---
+++ test/TranslateLibTest.cs
@@ -0,0 +1,30 @@
+using TranslateLiblary;
+
+namespace test;
+
+public class TranslateLibTest
+{
+    private Dictionary<string, string> _dict = new Dictionary<string, string>();
+    [Fact]
+    public void AddWordsTest()
+    {
+        // arrange
+        // act
+        // assert
+    }
+    [Fact]
+    public void CheckWordTest()
+    {
+        // arrange
+        // act
+        // assert
+    }
+    [Fact]
+    public void TranslatingTest()
+    {
+        // arrange
+        // act
+        // assert
+    }
+
+}
