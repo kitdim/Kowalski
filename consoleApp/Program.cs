29e7730c0ff4e1d9fb64cb8eff3eb852fb228311
kitdim <kitaykin9808@gmail.com>
2023-05-07 14:41:21 +0300

    Убрать отображение курсора после ответа Добавить ReadKey, для паузы, иначе не отображается вывод после ввода ответа

--- consoleApp/Program.cs
+++ consoleApp/Program.cs
@@ -5,14 +5,12 @@ internal class Program
     private static void Main()
     {
         Translate tutor = new();
-        tutor.AddWords("dog", "собака");
-        tutor.AddWords("cat", "кошка");
-        tutor.AddWords("door", "дверь");
 
         while (true)
         {
-            var checkWord = tutor.GetRandomEngWord();
-            Write("Как переводится слово {0}?: ", checkWord);
+            CursorVisible = true;
+            var checkWord = tutor.GetRandomEngWord() ?? default;
+            Write("Как переводится слово {0} ?: ", checkWord);
             var answer = ReadLine() ?? "пустая строка";
 
             if (tutor.CheckWord(checkWord, answer))
@@ -24,7 +22,8 @@ internal class Program
                 var currectAnswer = tutor.Translating(checkWord);
                 WriteLine("Неверно, слово {0} переводится как {1}.", checkWord, currectAnswer);
             }
-
+            CursorVisible = false;
+            ReadKey();
             Clear();
         }
     }
